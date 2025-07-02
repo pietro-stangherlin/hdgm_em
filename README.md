5e71999c057b9f3db7343a0c67e199e4ac74d499
pietro-stangherlin <pietro.stangherlin@gmail.com>
2025-07-02 11:55:13 +0200

    deleted many dfms folders and files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hdgm_em
+
+The template code is taken from "dfms: Dynamic Factor Models package" (https://cran.r-project.org/web/packages/dfms/index.html) of Sebastian Krantz [aut, cre], Rytis Bagdziunas [aut], Santtu Tikka [rev], Eli Holmes [rev].
--- dfms-development/misc/BM2014/RepFilesBanburaModugnoJAE14/functions/PC/monOfQ.m
+++
@@ -1,9 +0,0 @@
-function m = monOfQ(m)
-% function m = monOfQ(m)
-% Delivers the month within the quarter 
-% INPUT  m                      integer
-% OUTPUT m                   in {1,2,3}
-%______________________________________
-
-  m = mod(m,3);
-  m = m + 3*(m==0);
