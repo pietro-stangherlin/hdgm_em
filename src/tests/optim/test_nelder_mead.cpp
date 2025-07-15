29890126c934f4187e7a8086aa48421d04405ac4
pietro-stangherlin <pietrostangherlin@gmail.com>
2025-07-15 11:04:43 +0200

    first nelder mead test passed

--- src/tests/optim/test_nelder_mead.cpp
+++ src/tests/optim/test_nelder_mead.cpp
@@ -1,4 +1,5 @@
-#include "src/optim/nelder_mead.h"
+#include "optim/nelder_mead.h"
+#include <iostream>
 
 double function_to_minimize(const std::array<double,2>& x) {
   return x[0]*x[0] + x[1]*x[1];
@@ -6,17 +7,17 @@ double function_to_minimize(const std::array<double,2>& x) {
 
 int main() {
 
-  std::array<double,2> start = { 0.5, 0.5 };
+  std::array<double,2> start = { 5, 20 };
   std::array<double,2> step = { 0.1, 0.1 };
 
   nelder_mead_result<double,2> result = nelder_mead<double,2>(
     function_to_minimize,
     start,
-    1.0e-5, // the terminating limit for the variance of function values
+    1.0e-10, // the terminating limit for the variance of function values
     step
   );
   std::cout << "Found minimum: " << std::fixed << result.xmin[0] << ' ' << result.xmin[1] << std::endl;
-  std::cout<< "Expected (0,0)"
+  std::cout<< "Expected (0,0)";
 
   return 0;
 }
