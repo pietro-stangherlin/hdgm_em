b1b749d5e1277660e60b20cc3d86f78c63ac3315
pietro-stangherlin <pietrostangherlin@gmail.com>
2025-08-01 15:16:04 +0200

    removed R implementation of EM

---
+++ R/data_preprocessing.R
@@ -0,0 +1,10 @@
+# preprocessing functions
+# to convert data to pass to EM function
+
+# Assumptions:
+# 1) there is a fixed number of possible locations
+# and locations are marked by a qualitative variable
+# example: for each observation can be observed at most
+# locations a, b, c, d
+
+# 2) there can be missing data, in that case is still
