a188078b5e36081c23a88ab9e5188fd7a20d44be
pietro-stangherlin <pietrostangherlin@gmail.com>
2025-08-01 14:50:58 +0200

    removed unstructured EM from test_EM

--- tests/test_em.R
+++ tests/test_em.R
@@ -14,82 +14,7 @@ Rcpp::sourceCpp("src/em/EM_wrapper.cpp",
 #                 rebuild = TRUE)
 
 
-
-# Simulation function unstructured ----------------------------------------
-
-SimulEMUn <- function(B,
-                    n_times,
-                    transMatr, # true model parameters
-                    obsMatr,
-                    stateCovMatr,
-                    obsCovMatr,
-                    zeroState,
-                    em_transMatr, # starting EM values
-                    em_obsMatr,
-                    em_stateCovMatr,
-                    em_obsCovMatr,
-                    em_zeroState,
-                    em_zeroStateCov,
-                    em_max_iter = 50,
-                    em_bool_mat = TRUE,
-                    em_verbose = FALSE){
-
-  p = NROW(transMatr)
-  q = NROW(obsMatr)
-
-  Phi_hat_array <- array(NA, dim = c(p, p, B))
-  A_hat_array <- array(NA, dim = c(q, p, B))
-  Q_hat_array <- array(NA, dim = c(p, p, B))
-  R_hat_array <- array(NA, dim = c(q, q, B))
-  P0_smooth_hat_array <- array(NA, dim = c(p, p, B))
-  x0_hat_smooth <- matrix(NA, p, B)
-  llik_hat_vec <- rep(NA, B)
-
-
-  for(b in 1:B){
-
-    if(b %% 50 == 0){
-      print(paste0("simulation iter: ", b, collapse = ""))
-    }
-
-    y.matr <- LinGauStateSpaceSim(n_times = n_times,
-                                  transMatr = transMatr,
-                                  obsMatr = obsMatr,
-                                  stateCovMatr = stateCovMatr,
-                                  obsCovMatr = obsCovMatr,
-                                  zeroState = zeroState)$observations
-
-    res_un_EM <- UnstructuredEM(y = y.matr,
-                                Phi_0 = em_transMatr,
-                                A_0 = em_obsMatr,
-                                Q_0 = em_stateCovMatr,
-                                R_0 = em_obsCovMatr,
-                                x0_in = em_zeroState,
-                                P0_in = em_zeroStateCov,
-                                max_iter = em_max_iter,
-                                bool_mat = em_bool_mat,
-                                verbose = em_verbose)
-
-    Phi_hat_array[,,b] <- res_un_EM[["Phi"]]
-    A_hat_array[,,b] <- res_un_EM[["A"]]
-    Q_hat_array[,,b] <- res_un_EM[["Q"]]
-    R_hat_array[,,b] <- res_un_EM[["R"]]
-    P0_smooth_hat_array[,,b] <- res_un_EM[["P0_smoothed"]]
-    x0_hat_smooth[,b] <- res_un_EM[["x0_smoothed"]]
-
-  }
-
-  return(list("Phi" = Phi_hat_array,
-              "A" = A_hat_array,
-              "Q" = Q_hat_array,
-              "R" = R_hat_array,
-              "x0_smoothed" = x0_hat_smooth,
-              "P0_smoothed" = P0_smooth_hat_array))
-
-}
-
-
-# Simulation 1 -----------------------------------
+# Data generation -----------------------------------
 set.seed(123)
 
 N <- 10000
@@ -133,7 +58,7 @@ StateSpaceRes <- LinGauStateSpaceSim(n_times = N,
 y.matr <- StateSpaceRes$observations
 x.matr <- StateSpaceRes$states
 
-# Covariates ------------------------------------------------------------
+# Covariates generation ------------------------------------------------------------
 
 TRUE_FIXED_BETA <- c(2, -1, 4)
 
@@ -155,6 +80,10 @@ for(i in 1:N){
 }
 
 # Testing ---------------------------------
+
+# STRUCTURED -------------------------------------------------
+
+# NOT MISSING Y --------------------------------------------
 
 # Starting from true values -------------------------
 
@@ -181,50 +110,6 @@ plot(res_EM$par_history[2,], type = "l")
 plot(res_EM$par_history[3,], type = "l")
 plot(res_EM$par_history[4,], type = "l")
 plot(res_EM$par_history[5,], type = "l")
-
-# unstructured EM -------------------------------------
-res_un_EM = UnstructuredEM(y = y.matr,
-                           Phi_0 = G * diag(nrow = Y_LEN),
-                           A_0 = A * diag(nrow = Y_LEN),
-                           Q_0 = ETA_MATRIX,
-                           R_0 = SIGMAY^2 * diag(nrow = Y_LEN),
-                           x0_in = rep(0, Y_LEN),
-                           P0_in = ETA_MATRIX,
-                           max_iter = 200,
-                           bool_mat = FALSE,
-                           verbose = TRUE)
-
-res_un_EM$Phi
-res_un_EM$A
-res_un_EM$Q
-res_un_EM$R
-res_un_EM$x0_smoothed
-res_un_EM$P0_smoothed
-
-
-# simulation -------------------------------------------
-res_em_sim_true_start <- SimulEMUn(B = 300,
-                                   n_times = N,
-                                   transMatr = G * diag(nrow = Y_LEN),
-                                   obsMatr = A * diag(nrow = Y_LEN),
-                                   stateCovMatr = ETA_MATRIX,
-                                   obsCovMatr = SIGMAY^2 * diag(nrow = Y_LEN),
-                                   zeroState = rep(0, Y_LEN),
-                                   em_transMatr = G * diag(nrow = Y_LEN),
-                                   em_obsMatr = A * diag(nrow = Y_LEN),
-                                   em_stateCovMatr = ETA_MATRIX,
-                                   em_obsCovMatr = SIGMAY^2 * diag(nrow = Y_LEN),
-                                   em_zeroState = rep(0, Y_LEN),
-                                   em_zeroStateCov = ETA_MATRIX,
-                                   em_max_iter = 50,
-                                   em_bool_mat = TRUE,
-                                   em_verbose = FALSE)
-
-
-b = dim(res_em_sim_true_start$Phi)[3]
-
-plot(1:b, rep(A, b), type = "l")
-lines(res_em_sim_true_start$A[,,], col = "red")
 
 # Starting from not true values -------------------------
 # structured -------------------------------------
@@ -266,71 +151,6 @@ plot(res_EM_dist$par_history[4,1:res_EM_dist$niter], type = "l",
 abline(h = SIGMAY^2, col = "red")
 par(mfrow = c(1,1))
 
-# unstrucured ----------------------------------------------
-
-res_un_EM_dist = UnstructuredEM(y = y.matr,
-                           Phi_0 = 2 * G * diag(nrow = Y_LEN),
-                           A_0 = A * diag(nrow = Y_LEN),
-                           Q_0 = ETA_MATRIX,
-                           R_0 = SIGMAY^2 * diag(nrow = Y_LEN),
-                           x0_in = rep(0, Y_LEN),
-                           P0_in = 5 * diag(nrow = Y_LEN),
-                           max_iter = 500,
-                           bool_mat = FALSE,
-                           verbose = TRUE)
-
-res_un_EM_dist$Phi
-res_un_EM_dist$A
-res_un_EM_dist$Q
-res_un_EM_dist$R
-
-res_un_EM_dist$x0_smoothed
-
-
-res_em_sim_false_start <- SimulEMUn(B = 50,
-                                   n_times = N,
-                                   transMatr = G * diag(nrow = Y_LEN),
-                                   obsMatr = A * diag(nrow = Y_LEN),
-                                   stateCovMatr = ETA_MATRIX,
-                                   obsCovMatr = SIGMAY^2 * diag(nrow = Y_LEN),
-                                   zeroState = rep(0, Y_LEN),
-                                   em_transMatr = 2 * G * diag(nrow = Y_LEN),
-                                   em_obsMatr = A * diag(nrow = Y_LEN),
-                                   em_stateCovMatr = 2 * ETA_MATRIX,
-                                   em_obsCovMatr = 2 * SIGMAY^2 * diag(nrow = Y_LEN),
-                                   em_zeroState = 3 * rep(0, Y_LEN),
-                                   em_zeroStateCov = 3 *  ETA_MATRIX,
-                                   em_max_iter = 50,
-                                   em_bool_mat = FALSE,
-                                   em_verbose = FALSE)
-
-b = dim(res_em_sim_false_start$Phi)[3]
-
-plot(1:b, rep(A, b), type = "l", ylim = c(A - 4, A + 4))
-lines(res_em_sim_false_start$A[1,1,], col = "red")
-lines(res_em_sim_false_start$A[2,2,], col = "red")
-lines(res_em_sim_false_start$A[3,3,], col = "red")
-
-
-plot(1:b, rep(G, b), type = "l", ylim = c(G - 4, G + 4))
-lines(res_em_sim_false_start$Phi[1,1,], col = "red")
-lines(res_em_sim_false_start$Phi[2,2,], col = "red")
-lines(res_em_sim_false_start$Phi[3,3,], col = "red")
-
-plot(1:b, rep(0, b), type = "l", ylim = c(G - 4, G + 4))
-lines(res_em_sim_false_start$Phi[1,2,], col = "red")
-lines(res_em_sim_false_start$Phi[2,3,], col = "red")
-lines(res_em_sim_false_start$Phi[3,5,], col = "red")
-
-plot(1:b, rep(ETA_MATRIX[1,1], b), type = "l", ylim = c(G - 4, G + 4))
-lines(res_em_sim_false_start$Q[1,1,], col = "red")
-
-plot(1:b, rep((SIGMAY^2 * diag(nrow = Y_LEN))[1,1], b), type = "l", ylim = c(G - 4, G + 4))
-lines(res_em_sim_false_start$R[1,1,], col = "red")
-
-plot(1:b, res_em_sim_false_start$x0_smoothed[1,], type = "l")
-apply(res_em_sim_false_start$x0_smoothed, 1, mean)
-apply(res_em_sim_false_start$x0_smoothed, 1, sd)
 
 # Covariates ---------------------------
 # starting from true values ----------------------
